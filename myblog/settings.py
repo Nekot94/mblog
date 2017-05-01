49ba7e3443cfdb9199eb74e0a64e751961496179
Nekot94 <nekomato9@gmail.com>
2017-05-02 00:46:57 +0300

    test

--- myblog/settings.py
+++ myblog/settings.py
@@ -120,6 +120,12 @@ USE_TZ = True
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
+
+DEBUG = False
+
 
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
