62b26bd4e0428ac4d02fa0cda697f731c2c5262b
Nekot94 <nekomato9@gmail.com>
2017-05-02 00:21:14 +0300

    test

---
+++ posts/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, get_object_or_404
+
+from .models import Post
+
+# Create your views here.
+def post_list(request):
+    queryset = Post.objects.all()
+    context = {
+        "object_list": queryset,
+        "title": "Статьи",
+    }
+    return render(request, "post_list.html", context)
+
+
+def post_detail(request, id=None):
+    instance = get_object_or_404(Post, id=id)
+    context = {
+        "object": instance,
+        "title": instance.title,
+    }
+    return render(request, "post_detail.html", context)
