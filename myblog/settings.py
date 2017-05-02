91ebb59c88cff64f2b849d5b9e297dd22aa0d839
Nekot94 <nekomato9@gmail.com>
2017-05-02 22:24:42 +0300

    test5

--- myblog/settings.py
+++ myblog/settings.py
@@ -128,7 +128,9 @@ ALLOWED_HOSTS = ['*']
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'mediafiles')
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
--- posts/models.py
+++ posts/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 from django.core.urlresolvers import reverse
 
-# Create your models here.
+
+def upload_location(instance, filename):
+    return "%s/%s" %(instance.id,filename)
+
 class Posts(models.Model):
     title = models.CharField("Заголовок", max_length=120)
     content = models.TextField("Содержимое")
+    image = models.ImageField(upload_to=upload_location, null=True, blank=True)
     timestamp = models.DateTimeField("Создана", auto_now=False, auto_now_add=True)
     updated = models.DateTimeField("Обновлена", auto_now=True, auto_now_add=False)
 
